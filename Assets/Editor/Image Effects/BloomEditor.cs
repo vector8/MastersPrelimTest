594ea91efd0b569566ac4ead1f753aee9549f51a
Chris <vector8@users.noreply.github.com>
2018-09-17 14:57:23 -0400

    Prelim fix to teleporting, need to test. Updated version and added ignores.

--- Assets/Editor/Image Effects/BloomEditor.cs
+++ Assets/Editor/Image Effects/BloomEditor.cs
@@ -82,7 +82,7 @@ class BloomEditor : Editor
 		// display info text when screen blend mode cannot be used
 		Camera cam = (target as Bloom).GetComponent<Camera>();
 		if (cam != null) {
-			if (screenBlendMode.enumValueIndex==0 && ((cam.hdr && hdr.enumValueIndex==0) || (hdr.enumValueIndex==1))) {
+			if (screenBlendMode.enumValueIndex==0 && ((cam.allowHDR && hdr.enumValueIndex==0) || (hdr.enumValueIndex==1))) {
 				EditorGUILayout.HelpBox("Screen blend is not supported in HDR. Using 'Add' instead.", MessageType.Info);
 			}
 		}
